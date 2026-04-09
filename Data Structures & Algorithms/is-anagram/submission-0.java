48788e7ad0fffcc3daa619201213198834462e99
TaeinUm <131139887+TaeinUm@users.noreply.github.com>
2026-04-09 09:26:52 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+        if (Arrays.equals(sc, tc))
+            return true;
+        
+        return false;
+    }
+}
